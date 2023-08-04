f25999343aeee1f8c76bafc454664abe96635cf6
ahmed_korany <a.Korany@elham.sa>
2023-08-04 23:17:04 +0300

    implement step2

--- glameraTask/src/app/components/auth/auth-routing.module.ts
+++ glameraTask/src/app/components/auth/auth-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
+import { Step2Component } from './step2/step2.component';
 
 const routes: Routes = [
   {
     path: 'step1',
     component: RegistrationComponent,
+  },
+  {
+    path: 'step2',
+    component: Step2Component,
   },
 ];
 
